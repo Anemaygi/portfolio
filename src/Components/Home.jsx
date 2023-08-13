ddf1cac347dc38ca993ed5eaa72ac7f6ed8ffa64
Giane Mayumi <62656745+Anemaygi@users.noreply.github.com>
2023-08-13 03:32:52 +0000

    remove link

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -36,7 +36,6 @@ const Home = ({ name, title }) => {
       <div style={{ position: "absolute", top: "30%", left: "2rem" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
-        <h3 style={headingStyle}>Find me on <a href="https://www.linkedin.com/in/gianemayumi/">LinkedIn</a> or <a href="https://github.com/Anemaygi">Github</a></h3>
       </div>
       <div style={{ position: "absolute", bottom: "8rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
