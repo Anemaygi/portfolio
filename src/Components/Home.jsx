11c701e151e7aea39565092f1eb0b436524a0e7c
Giane Mayumi <62656745+Anemaygi@users.noreply.github.com>
2023-08-13 03:36:25 +0000

    finishing

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,14 +21,11 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/server-wall.jpg";
+import image from "../images/bg1.jpg";
 
 const imageAltText = "woman holding laptop standing by server room with glass wall";
 
 const Home = ({ name, title }) => {
-  const headingStyle = {
-    textAlign: 'center', // Use camelCase for CSS properties
-  };
   
   return (
     <section id="home" className="dark">
